0c486a88a6d7c492b24bffd9e1980a53ce5f5359
Kpeters <kaitlynpeters@cox.net>
2023-09-17 18:38:45 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 function App() {
   return (
       <Header />
@@ -11,5 +13,83 @@ function Header() {
   )
 }
 
+
+function ItemsTable({ items }) {
+  const rows = [];
+
+  items.forEach((item) => {
+    
+      rows.push(
+        <ItemRows />
+      );
+  });
+}
+
+
+function ItemRows( { item }) {
+  //get props from the user and display in row
+  const name = item.name;
+  
+
+  return(
+    <tr>
+      <td>
+        <input type="checkBox" id="task"></input>
+      </td> 
+      <td>{name}</td>
+      <td></td>
+    </tr>
+  );
+}
+
+function ListCategoryRow() {
+  //probably completed/uncompleted for now
+
+}
+
+// entire component of list
+function ToListWidget({ items }){
+  //table: completed, title, date
+  const [completed, setCompleted] = useState(false);
+  const [currentItems, setCurrentItems] = useState();
+  const [typingState, setTypingState] = useState('typiing');
+  const [newTask, setNewTask] = useState('');
+
+  // handle submit
+  
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+function AddListItems() {
+  //input bar, "+"/"add" button
+
+  function handleTextareaChange(e) {
+    setNewTask(e.target.Value);
+  }
+
+  return(
+    <>
+    <form onSubmit={handleSubmit}>
+    {/* input */}
+    <textarea
+      value={answer}
+      onChange={handleTextareaChange}
+      disabled={status === 'submitting'}
+      />
+      <br />
+      {/* button */}
+      <button disabled={
+        newTask.length === 0 ||
+        status === 'submitting'
+      }> Add Task
+      </button>
+      </form>
+    </>
+  );
+}
 
 export default App;
