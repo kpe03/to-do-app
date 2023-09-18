9668b1f659348feee0a7c648db6a8bb02cacaa2b
KPeters <99045807+kpe03@user.noreply.github.com>
2023-09-17 23:40:27 -0500

    changed method, of displaying tasks.

--- src/App.js
+++ src/App.js
@@ -1,95 +1,165 @@
-import {useState} from 'react';
+import {useState, useContext, useReducer, createContext } from 'react';
 
-function App() {
+export default function App() {
   return (
-      <Header />
+    <TasksProvider>
+      <h1>To Do List</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
   );
 }
-function Header() {
-  return(
-    <header>
-      <h2>To Do List</h2>
-    </header>
-  )
-}
+//context for tasks
+const TasksContext = createContext(null); 
+const TasksDispatchContext = createContext(null);
 
-
-function ItemsTable({ items }) {
-  const rows = [];
-
-  items.forEach((item) => {
-    
-      rows.push(
-        <ItemRows />
-      );
-  });
-}
-
-
-function ItemRows( { item }) {
-  //get props from the user and display in row
-  const name = item.name;
+//CONTEXTS AND REDUCERS ??
+function TasksProvider({ children }) {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
   
-
   return(
-    <tr>
-      <td>
-        <input type="checkBox" id="task"></input>
-      </td> 
-      <td>{name}</td>
-      <td></td>
-    </tr>
+    <TasksContext.Provider value={tasks}>
+      <TasksDispatchContext.Provider
+        value={dispatch}
+      >
+        {children}
+      </TasksDispatchContext.Provider>
+    </TasksContext.Provider>
   );
 }
-
-function ListCategoryRow() {
-  //probably completed/uncompleted for now
-
+function useTasks() {
+  return useContext(TasksContext);
 }
-
-// entire component of list
-function ToListWidget({ items }){
-  //table: completed, title, date
-  const [completed, setCompleted] = useState(false);
-  const [currentItems, setCurrentItems] = useState();
-  const [typingState, setTypingState] = useState('typiing');
-  const [newTask, setNewTask] = useState('');
-
-  // handle submit
-  
-  return(
-    <div>
-
-    </div>
-  )
+function useTasksDispatch() {
+  return useContext(TasksDispatchContext);
 }
-
-function AddListItems() {
-  //input bar, "+"/"add" button
-
-  function handleTextareaChange(e) {
-    setNewTask(e.target.Value);
+function tasksReducer(tasks, action) {
+  switch(action.type) {
+    case 'added': {
+      return[...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false
+      }];
+    }
+    case 'changed': {
+      return tasks.map(t => {
+        if(t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter(t=> t.id !== action.id);
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
   }
+}
 
+//COMPONENTS OF APP!!
+function TaskList() {
+  const tasks = useTasks();
   return(
-    <>
-    <form onSubmit={handleSubmit}>
-    {/* input */}
-    <textarea
-      value={answer}
-      onChange={handleTextareaChange}
-      disabled={status === 'submitting'}
+    <ul>
+      {tasks.map(task=> (
+        <li key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+function Task({ task }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const dispatch = useTasksDispatch();
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input
+          value={task.text}
+          onChange={e => {
+            dispatch({
+              type: 'changed',
+              task: {
+                ...task,
+                text: e.target.value
+              }
+            });
+          }} />
+        <button onClick={() => setIsEditing(false)}>
+          Save
+        </button>
+      </>
+    );
+  } else {
+    taskContent = (
+      <>
+        {task.text}
+        <button onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={e => {
+          dispatch({
+            type: 'changed',
+            task: {
+              ...task,
+              done: e.target.checked
+            }
+          });
+        }}
       />
-      <br />
-      {/* button */}
-      <button disabled={
-        newTask.length === 0 ||
-        status === 'submitting'
-      }> Add Task
+      {taskContent}
+      <button onClick={() => {
+        dispatch({
+          type: 'deleted',
+          id: task.id
+        });
+      }}>
+        Delete
       </button>
-      </form>
+    </label>
+  );
+}
+
+
+function AddTask({ onAddTask }) {
+  const [text, setText] = useState('');
+  const dispatch = useTasksDispatch();
+  return (
+    <>
+      <input
+        placeholder="Add task"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button onClick={() => {
+        setText('');
+        dispatch({
+          type: 'added',
+          id: nextId++,
+          text: text,
+        });
+      }}>Add</button>
     </>
   );
 }
 
-export default App;
+let nextId = 1;
+
+const initialTasks = [
+  {id: 0, text: 'Homework', done: false}
+];
+
